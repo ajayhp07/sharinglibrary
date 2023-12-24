b8d08983aff9118adea3e3d6efa1291287ca1f95
AJAY PATEL <99001529+ajayhp07@users.noreply.github.com>
2023-12-24 16:58:31 +0530

    Update ajay.groovy

--- vars/ajay.groovy
+++ vars/ajay.groovy
@@ -1,1 +1,19 @@
+def git(repo)
+{
+  git'https://github.com/ajayhp07/${repo}git'
+}
 
+def mvn()
+{
+  sh'mvn package'
+}
+
+def deploy(jobname,ip,context)
+{
+  sh'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war '
+}
+
+def test(jobname)
+{
+  sh'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
+}
